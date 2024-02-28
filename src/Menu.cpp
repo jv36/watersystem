9f71912c863978bfdaffbbea4a3b287db810b125
João Vicente <up202208586@up.pt>
2024-02-28 14:55:47 +0000

    Here we go again!

---
+++ src/Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joaomendes on 28-02-2024.
+//
+
+#include "Menu.h"
---
+++ src/Menu.h
@@ -0,0 +1,8 @@
+//
+// Created by joaomendes on 28-02-2024.
+//
+
+#ifndef WATERSYSTEM_MENU_H
+#define WATERSYSTEM_MENU_H
+
+#endif //WATERSYSTEM_MENU_H
